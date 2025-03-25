1790042cca91de7cd896bccfcbc867aa30127b97
Codey Gallup <codey.gallup@gmail.com>
2025-03-25 10:07:54 -0600

    Spring initalzr

---
+++ snipperapp/src/test/java/com/codey/snipperapp/SnipperappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codey.snipperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
